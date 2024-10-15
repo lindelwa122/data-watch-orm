666d15f5ab8efa05ad4a29de387d6a583af328b4
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-10-15 19:34:00 +0530

    feat(fetch):added first function

--- schemas/manager.py
+++ schemas/manager.py
@@ -1,7 +1,27 @@
+import sqlite3
+from helpers.helper import first
+
 class Manager:
-    def __init__(self, model):
+    def __init__(self, model, db_path=':memory:'):
+        """
+        Initialize the Manager with a model and database path.
+        
+        :param model: The model class this manager is associated with
+        :param db_path: Path to the SQLite database file, defaults to in-memory database
+        """
         self._model = model
-        self._data = []
+        self._db_path = db_path
+        self._conn = None
+        self._cursor = None
+
+    def _ensure_connection(self):
+        """
+        Ensure that a database connection exists, creating one if it doesn't.
+        This lazy initialization helps in resource management.
+        """
+        if not self._conn:
+            self._conn = sqlite3.connect(self._db_path)
+            self._cursor = self._conn.cursor()
 
     def all(self):
         # todo
@@ -48,12 +68,18 @@ class Manager:
         pass
 
     def last(self):
-        # todo 
+        # todo
         pass
 
     def first(self):
-        # todo
-        pass
+        self._ensure_connection()
+        
+        table_name = self._model.__name__.lower()
+        row = first(self._cursor, table_name)
+        if row:
+            column_dict = dict(zip([column[0] for column in self._cursor.description], row))
+            return self._model(**column_dict)
+        return None
 
     def order_by(self):
         # todo
@@ -74,3 +100,10 @@ class Manager:
     def save(self):
         # todo
         pass
+
+    def __del__(self):
+        """
+        Destructor to ensure the database connection is closed when the Manager instance is deleted.
+        """
+        if self._conn:
+            self._conn.close()
